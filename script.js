b4b8c5484ee2972d7346232040702049c10775e0
Rodgersouko <gabrielaouko@gmail.com>
2020-09-06 08:42:10 +0300

    creating object for user order

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 $(document).ready(function() {
-      $("#form").submit(function(event) {
+    $("#form").submit(function (event) {
         //functions to get user input from the forms
         function flavor() {
-          var pizzaFlavour = document.getElementById("flavor").value;
-          return parseInt(pizzaFlavour);
+            var pizzaFlavour = document.getElementById("flavor").value;
+            return parseInt(pizzaFlavour);
         }
     
         function size() {
-          var pizzaSize = document.getElementById("size").value;
-          return parseInt(pizzaSize);
+            var pizzaSize = document.getElementById("size").value;
+            return parseInt(pizzaSize);
         }
         function crust() {
-          var pizzaCrust = document.getElementById("crust").value;
-          return parseInt(pizzaCrust);
+            var pizzaCrust = document.getElementById("crust").value;
+            return parseInt(pizzaCrust);
         }
         function topping() {
-          var pizzaTopping = document.getElementById("topping").value;
-          return parseInt(pizzaTopping);
+            var pizzaTopping = document.getElementById("topping").value;
+            return parseInt(pizzaTopping);
         }
         function number() {
-          var pizzaNumber = document.getElementById("quantity").value;
-          return parseInt(pizzaNumber);
-    }
+            var pizzaNumber = document.getElementById("quantity").value;
+            return parseInt(pizzaNumber);
+        }
+        //object for user order
+        function Order(flavor, size, crust, topping, quantity) {
+            this.newFlavor = flavor;
+            this.newSize = size;
+            this.newCrust = crust;
+            this.newTopping = topping;
+            this.newQuantity = quantity;
+        }
 
-      }
+    });
+});
