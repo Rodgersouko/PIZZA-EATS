796cde1600e5f383e679f63e870efe1c1e515d5d
Rodgersouko <gabrielaouko@gmail.com>
2020-09-06 08:20:27 +0300

    created function for getting users input

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+      $("#form").submit(function(event) {
+        //functions to get user input from the forms
+        function flavor() {
+          var pizzaFlavour = document.getElementById("flavor").value;
+          return parseInt(pizzaFlavour);
+        }
+    
+        function size() {
+          var pizzaSize = document.getElementById("size").value;
+          return parseInt(pizzaSize);
+        }
+        function crust() {
+          var pizzaCrust = document.getElementById("crust").value;
+          return parseInt(pizzaCrust);
+        }
+        function topping() {
+          var pizzaTopping = document.getElementById("topping").value;
+          return parseInt(pizzaTopping);
+        }
+        function number() {
+          var pizzaNumber = document.getElementById("quantity").value;
+          return parseInt(pizzaNumber);
+    }
+
+      }
